ce7bb95ff90b2c8c4a4dc483d7ad795239e26bcc
Annie-Jeevas <anna.light.terry@gmail.com>
2016-12-24 22:50:07 +0300

    24.12.2016 - добавляется два коммента

---
+++ src/java/Beans/SessionBean.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import model.*;
+import dao.*;
+import java.security.Principal;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.context.FacesContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author Анюта
+ */
+public class SessionBean {
+
+    private final Principal principal = FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+    public int idBookForReading;
+    private Book ReadingBook;
+    private ArrayList<Comment> Comments;
+    private Comment addedComm;
+    private final DAOBook daob = new DAOBook();
+
+    public Principal getPrincipal() {
+        return principal;
+    }
+
+    
+    
+    public int getIdBookForReading() {
+        try {
+            getReadingBook();
+            getComments();
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            return idBookForReading;
+        }
+    }
+
+    public String setIdBookForReading(int idBookForReading) {
+        this.idBookForReading = idBookForReading;
+        return "case1";
+    }
+
+    public Book getReadingBook() throws SQLException, ClassNotFoundException {
+        try 
+        {
+            Book b = new Book(idBookForReading);
+            ReadingBook = daob.read(b).get(0);
+            //addedComm.setCommentedBook(ReadingBook);
+            //b.setName("aaaaaa");
+            //ReadingBook = b;
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            return ReadingBook;
+        }
+    }
+
+    public void setReadingBook(Book ReadingBook) {
+        this.ReadingBook = ReadingBook;
+    }
+
+    public ArrayList<Comment> getComments() {
+
+        try {
+            DAOComment daoc = new DAOComment();
+            Comments = daoc.read(ReadingBook);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            return Comments;
+        }
+    }
+
+    public void setComments(ArrayList<Comment> Comments) {
+        this.Comments = Comments;
+    }
+
+    public Comment getAddedComm() {
+        return addedComm;
+    }
+
+    public void setAddedComm(Comment addedComm) {
+        this.addedComm = addedComm;
+    }
+
+    public String add() {
+        try {
+            //addedComm = new Comment();
+            String username;
+            if (principal!=null){
+            username = principal.getName();}
+            else { username = "anya";}
+            DAOUser daou = new DAOUser();
+            DAOReader daor = new DAOReader();
+            User user = daou.getUserByUsername(username);
+            Reader commentAuthor = daor.read(user.getId_visitor()).get(0);
+            addedComm.setCommentAuthor(commentAuthor);
+            DAOBook daob = new DAOBook();
+            Book b = new Book(idBookForReading);
+            b = daob.read(b).get(0);
+            addedComm.setCommentedBook(b);
+            addedComm.setStatus("Составлен");
+            DAOComment daoc = new DAOComment();
+            daoc.create(addedComm);
+            return "ok";
+        } catch (ClassNotFoundException | SQLException | NamingException ex) {
+            Logger.getLogger(SessionBean.class.getName()).log(Level.SEVERE, null, ex);
+            return "fail";
+        }
+    }
+
+    public SessionBean() {
+
+        addedComm = new Comment();
+    }
+
+}
